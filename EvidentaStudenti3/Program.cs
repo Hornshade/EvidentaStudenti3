1cd91dbc5188c9d83b6cdccd05b08e05d85df6d1
Hornshade <sabin.mihai@yahoo.com>
2020-04-03 13:00:10 +0300

    First Commit

---
+++ EvidentaStudenti3/Program.cs
@@ -0,0 +1,78 @@
+﻿using LibrarieEntitati;
+using System;
+
+
+namespace ManagementStudenti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student("Ionescu");
+            //s.SetNote("");
+
+            int[] noteStd;
+            noteStd = new int[5];
+            int linii = 4; 
+            string[][] noteTablou = new string[linii][];
+            for (int i = 0; i < linii; i++)
+                noteTablou[i] = new string[0];
+            Student tab = new Student("Tablou");
+
+
+            bool validare = true;
+            while (validare)
+            {
+                Console.Clear();
+                Console.WriteLine("S.Citeste note de la tastatura ca un sir de caractere");
+                Console.WriteLine("T.Citeste note de la tastatura(cate o nota pe rand)");
+                Console.WriteLine("C.Citeste note din linia de comanda");
+                Console.WriteLine("A.Afisare note");
+                Console.WriteLine("I.Info autor");
+                Console.WriteLine("X.Iesire.");
+                Console.WriteLine("\nSelectati o optiune : ");
+
+
+                switch (Console.ReadLine())
+                {
+                    case "S":
+                        Console.WriteLine("Introduceti notele ca un sir de caractere: ");
+                        string notes = Console.ReadLine();
+                        tab.SetNote(notes);
+                        tab.ConversieLaSir();
+                        Console.ReadKey();
+                        break;
+                    case "T":
+                        Console.WriteLine("Introduceti notele de la tastatura(cate o nota pe rand):");
+                        int[] nota;
+                        for (int i = 0; i < linii; i++)
+                        {
+                            Console.WriteLine("Introduceti numarul de note din anul {0}", i);
+                            int n = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Introduceti cate o nota pe rand :");
+                            nota = new int[n];
+                            for (int j = 0; j < n; j++)
+                            {
+                                nota[j] = Convert.ToInt32(Console.ReadLine());
+                            }
+                            tab.SetNoteRand(nota, n, i);
+                        }
+                       
+                        Console.ReadKey();
+                        break;
+                    case "C":
+                        string NoteCmd = "Nu exista note in linia de comanda";
+                        NoteCmd = string.Join(" ", args);
+                        Console.WriteLine(NoteCmd);
+                        Console.ReadKey();
+                        break;
+                    default:
+                        validare = false;
+                        break;
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ LibrarieEntitati/Student.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace LibrarieEntitati
+{
+    public class Student
+    {
+        // date membre private
+        string nume;
+        int[][] note = new int[4][];
+
+        // contructor implicit
+        public Student()
+        {
+            nume = string.Empty;
+        }
+
+        // constructor cu parametri
+        public Student(string _nume)
+        {
+            nume = _nume;
+            
+        }
+
+        public void SetNote(string sirNote)
+        {
+
+            // initializare vector cu note
+            string[] note_split = sirNote.Split(',');                   //primeste format 1 2 3 4,4 5 6 7,3 4 2,1 3 si desparte la virgula pentru ani;
+            // DE IMPLEMENTAT:
+            // extrage note din sir si adauga in vectorul note
+            
+            string[] splitNota;
+            int contor_an = 0;
+            
+            foreach (var nota in note_split)
+            {
+                
+                int contor_coloana = 0;
+                splitNota = nota.Split(' ');
+                note[contor_an] = new int[splitNota.Length];     //lungimea notelor din primul an 
+                foreach (var nota_an in splitNota)
+                {
+                    
+                    note[contor_an][contor_coloana] = Convert.ToInt32(nota_an);
+
+                    contor_coloana++;
+                }
+                contor_an++;
+            } 
+        }
+
+        public void SetNoteRand(int[] note,int nrnote,int an)
+        {
+            Array.Resize(ref this.note[an], this.note[an].Length + nrnote);
+
+            for(int i=0;i<nrnote; i++)
+            {
+                this.note[an][i] = note[i];
+            }
+        }
+        public void ConversieLaSir()
+        {
+            string afisare;
+            Console.WriteLine("Studentul {0} are notele : ...", nume);
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Notele din anul {0}", i + 1);
+                afisare = string.Join(",", note[i]);
+                Console.WriteLine(afisare);
+
+            }
+        }
+
+
+    }
+}
